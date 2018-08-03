d2fa7ca0b7ec1a114a1b0d4661ede8edca5f5dca
Izabela Soares <izabelasoares001@gmail.com>
2018-08-03 09:13:39 -0300

    Single jump + correcoes do feedback

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -21,16 +21,34 @@ public class PlayerCtrl : MonoBehaviour {
 	Animator anim;
 
 	bool isJumping = false;
+
+	public Transform feet;
+
+	public float feetWidth = 0.5f;
+
+	public float feetHeight = 0.1f;
+
+	public bool isGrounded;
+
+	public LayerMask whatIsGround;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		sr = GetComponent<SpriteRenderer>();
 		anim = GetComponent<Animator>();
 	}
+
+	void OnDrawGizmos(){
+		Gizmos.DrawWireCube(feet.position, new Vector3(feetWidth, feetHeight, 0f));
+
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		isGrounded = Physics2D.OverlapBox(new Vector2(feet.position.x, feet.position.y), new Vector2(feetWidth, feetHeight), 360.0f, whatIsGround);
+
 		float horizontalInput = Input.GetAxisRaw("Horizontal"); // -1: esquerda, 1: direita
 		float horizontalPlayerSpeed = horizontalSpeed * horizontalInput;
 		if (horizontalPlayerSpeed != 0){
@@ -76,9 +94,11 @@ public class PlayerCtrl : MonoBehaviour {
 	}
 
 	void Jump() {
+		if(isGrounded){
 		isJumping = true;
 		rb.AddForce(new Vector2(0f, jumpSpeed));
 		anim.SetInteger("State", 1);
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D other) {
