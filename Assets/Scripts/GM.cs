7817c17fa52d4eb133c718a048f5c4b37376e73f
Izabela Soares <izabelasoares001@gmail.com>
2018-08-27 16:33:06 -0300

    Contando vidas

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -62,6 +62,7 @@ public class GM : MonoBehaviour {
 
 	void DisplayHudData() {
 		ui.hud.txtCoinCount.text = "x " + data.coinCount;
+		ui.hud.txtLifeCount.text = "x " + data.lifeCount;
 		ui.hud.txtTimer.text = "Timer: " + timeLeft.ToString("F1");
 	}
 
@@ -73,10 +74,19 @@ public class GM : MonoBehaviour {
 		Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
 	}
 
+	public void DecrementLives(){
+		data.lifeCount--;
+	}
 	public void KillPlayer(){
 		if(player != null){
 			Destroy(player.gameObject);
-			Invoke("RespawnPlayer", timeToRespawn);
+			DecrementLives();
+			if (data.lifeCount > 0){
+				Invoke("RespawnPlayer", timeToRespawn);
+			}
+			else {
+				GameOver();
+			}
 		}
 	}
 
