a893311a6f2fbaaa2541383200ae71ce3b39e434
Izabela Soares <izabelasoares001@gmail.com>
2018-09-04 15:56:03 -0300

    Tocando som fail e controlando a musica

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -24,4 +24,8 @@ public void PlayJumpSound(GameObject obj){
 	AudioSource.PlayClipAtPoint(audioPlayer.jump, obj.transform.position);
 }
 
+public void PlayFailSound(GameObject obj){
+	AudioSource.PlayClipAtPoint(audioSFX.fail, obj.transform.position);
+}
+
 }
